df234eb5cf2c65bf9a397a0f63ad478bb8dc6ef1
Gustavo Oliveira <g.oliveira0418@gmail.com>
2021-08-20 19:40:56 -0300

    Created controller's tests

--- src/main/java/com/example/viaCep/ViaCepApplicationDuo2Application.java
+++ src/main/java/com/example/viaCep/ViaCepApplicationDuo2Application.java
@@ -1,13 +1,25 @@
 package com.example.viaCep;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.viaCep.model.AdressEntity;
+import com.example.viaCep.repository.AdressRepository;
 
 @SpringBootApplication
 public class ViaCepApplicationDuo2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ViaCepApplicationDuo2Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(AdressRepository repository) {
+		return (args) -> {
+			repository.save(new AdressEntity("123", "teste", "teste1", "teste2", "teste3", "teste4", "teste5", "teste6", 123, "teste8"));
+		};
 	}
 
 }
--- src/test/java/com/example/viaCep/ViaCepApplicationDuo2ApplicationTests.java
+++ src/test/java/com/example/viaCep/ViaCepApplicationDuo2ApplicationTests.java
@@ -1,13 +1,39 @@
 package com.example.viaCep;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ViaCepApplicationDuo2ApplicationTests {
-
+	
+	@Autowired
+	private MockMvc mvc;
+	
 	@Test
-	void contextLoads() {
+	void testGetAdressSuccess() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/api/adresses/zip/123")
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(content().contentType("application/json;charset=UTF-8"))
+		.andExpect(content().json("{\"zip\":\"123\",\"publicPlace\":\"teste\",\"complements\":\"teste1\",\"district\":\"teste2\",\"city\":\"teste3\",\"uf\":\"teste4\",\"ibge\":\"teste5\",\"gia\":\"teste6\",\"ddd\":123,\"siafi\":\"teste8\"}"));		
+	}
+	
+	@Test
+	void testGetAdressNotFound() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/api/adresses/zip/")
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isNotFound());
 	}
 
 }
+
+
