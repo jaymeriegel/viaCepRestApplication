bde9ecfbec56f161ec5ecbb57d19b881271aa8ea
Jayme Gomes <jayme.r.g.n@hotmail.com>
2021-08-20 17:07:25 -0300

    first commit

---
+++ src/main/java/com/example/viaCep/ViaCepApplicationDuo2Application.java
@@ -0,0 +1,13 @@
+package com.example.viaCep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaCepApplicationDuo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViaCepApplicationDuo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/viaCep/ViaCepApplicationDuo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.viaCep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViaCepApplicationDuo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
