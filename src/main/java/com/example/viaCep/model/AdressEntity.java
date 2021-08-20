38d37ab99a7e6a40ab9edf5f07031780ecdf5450
Jayme Gomes <jayme.r.g.n@hotmail.com>
2021-08-20 17:28:42 -0300

    add adressrepository and adressentity v1

---
+++ src/main/java/com/example/viaCep/model/AdressEntity.java
@@ -0,0 +1,132 @@
+/**
+ * 
+ */
+package com.example.viaCep.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author Jayme
+ *
+ */
+
+@Entity
+public class AdressEntity {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long zip;
+	private String publicPlace;
+	private String complements;
+	private String district;
+	private String city;
+	private String uf;
+	private String ibge;
+	private String gia;
+	private Integer ddd;
+	private String siafi;
+	
+	public AdressEntity() {}
+	
+	/**
+	 * @param zip
+	 * @param publicPlace
+	 * @param complements
+	 * @param district
+	 * @param city
+	 * @param uf
+	 * @param ibge
+	 * @param gia
+	 * @param ddd
+	 * @param siafi
+	 */
+	public AdressEntity(Long zip, String publicPlace, String complements, String district, String city, String uf,
+			String ibge, String gia, Integer ddd, String siafi) {
+		super();
+		this.zip = zip;
+		this.publicPlace = publicPlace;
+		this.complements = complements;
+		this.district = district;
+		this.city = city;
+		this.uf = uf;
+		this.ibge = ibge;
+		this.gia = gia;
+		this.ddd = ddd;
+		this.siafi = siafi;
+	}
+
+	/**
+	 * @return the zip
+	 */
+	public Long getZip() {
+		return zip;
+	}
+	
+	/**
+	 * @return the publicPlace
+	 */
+	public String getPublicPlace() {
+		return publicPlace;
+	}
+	
+	/**
+	 * @return the complements
+	 */
+	public String getComplements() {
+		return complements;
+	}
+	
+	/**
+	 * @return the district
+	 */
+	public String getDistrict() {
+		return district;
+	}
+	
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+	
+	/**
+	 * @return the uf
+	 */
+	public String getUf() {
+		return uf;
+	}
+	
+	/**
+	 * @return the ibge
+	 */
+	public String getIbge() {
+		return ibge;
+	}
+	
+	/**
+	 * @return the gia
+	 */
+	public String getGia() {
+		return gia;
+	}
+	
+	/**
+	 * @return the ddd
+	 */
+	public Integer getDdd() {
+		return ddd;
+	}
+	
+	/**
+	 * @return the siafi
+	 */
+	public String getSiafi() {
+		return siafi;
+	}
+	
+	
+}
---
+++ src/main/java/com/example/viaCep/repository/AdressRepository.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.example.viaCep.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.viaCep.model.AdressEntity;
+
+/**
+ * @author Jayme
+ *
+ */
+public interface AdressRepository extends CrudRepository<AdressEntity, Long> {
+	
+	AdressEntity findByCep(String cep);
+
+}
